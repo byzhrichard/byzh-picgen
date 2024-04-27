2643d63cbfa3fdc08f9d3e4672baeb980c052acf
byzh <805278087@qq.com>
2024-04-27 21:36:55 +0800

    awa

--- src/main/java/com/byzh/picgen/test/picgen.java
+++ src/main/java/com/byzh/picgen/test/picgen.java
@@ -7,31 +7,25 @@ import java.io.IOException;
 import java.util.Random;
 
 public class picgen {
+
+
     public static void main(String[] args) throws IOException {
-        mytry();
+        String path = "src/main/resources/assets/byzh-picgen/textures/block/";
+        mytry("byzh_red",0xFF0000,path);
     }
-    private static void mytry() throws IOException {
+    private static void mytry(String name,int pic,String path) throws IOException {
 
         int width = 16; // 画布宽度
         int height = 16; // 画布高度
-        int[][] pixels = new int[width][height]; // 初始化画布
-
-
-        int byzh_red = 0xFF;
-        int byzh_green = 0x00;
-        int byzh_blue = 0x00;
-        int pixel = (byzh_red << 16) | (byzh_green << 8) | byzh_blue; // 合并RGB值
-        System.out.println(pixel);
-        System.out.println("awa");
 
         BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
         for (int x = 0; x < width; x++) {
             for (int y = 0; y < height; y++) {
-                image.setRGB(x, y, pixel); // 设置像素颜色
+                image.setRGB(x, y,pic); // 设置像素颜色
             }
         }
 
-        File output = new File("out.png");
+        File output = new File(path + name +".png");
         ImageIO.write(image, "png", output); // 保存为PNG格式的图片
     }
 }
