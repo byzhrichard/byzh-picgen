1da6288c311e9b148195f13d2c573caf4b3dbcd5
byzh <805278087@qq.com>
2024-04-27 18:01:48 +0800

    awa

---
+++ src/main/java/com/byzh/picgen/ByzhPicgenClient.java
@@ -0,0 +1,10 @@
+package com.byzh.picgen;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ByzhPicgenClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
