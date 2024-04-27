c62fa47bd26d6f75ad595a3206e8dac055c04be9
byzh <805278087@qq.com>
2024-04-27 17:54:51 +0800

    awa

---
+++ src/main/java/com/byzh/picgen/ByzhPicgenDataGenerator.java
@@ -0,0 +1,11 @@
+package com.byzh.picgen;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ByzhPicgenDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
